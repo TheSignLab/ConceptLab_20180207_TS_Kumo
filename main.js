240c5f16652f39bd814e8b725eeb3f71da89939e
TheSignLab Co <34943861+TheSignLab@users.noreply.github.com>
2018-06-02 17:59:28 -0500

    sdfg
    sdfgsdfg

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var Crawler = require("crawler");
 var fs = require('fs');
-
+var k = 0;
 
 var url = "http://busquedas.dafp.gov.co/search?btnG=Buscar&client=Hojas_de_vida&output=xml_no_dtd&proxystylesheet=Hojas_de_vida&sort=date:D:L:d1&oe=UTF-8&ie=UTF-8&ud=1&getfields=*&wc=200&wc_mc=1&lr=lang_es&exclude_apps=1&site=Hojas_de_Vida&filter=0&entqr=3&ulang=es&ip=190.147.24.37&access=p&entqrm=0&q=bogota+inmeta:DPTO%3DBogot%C3%A1%2520D%252EC&dnavs=inmeta:DPTO%3DBogot%C3%A1%2520D%252EC&start=0";
 process.stdout.write('\033c');
@@ -56,15 +56,19 @@ var c = new Crawler({
                 }
                 fs.writeFile('./output.json', JSON.stringify(arrayOfObjects), 'utf-8', function (err) {
                     if (err) throw err
-                    console.log('Done!')
+                   
                 })
             });
             
             if($(".b a")){
                var nextPageLink = $(".b a").attr("href");
                 setQueueNext("http://busquedas.dafp.gov.co/"+nextPageLink);
+                k = k +1;
+               console.log(nextPageLink )
             }
            else{
+                var nextPageLink = $(".b a").attr("href");
+               console.log(nextPageLink )
               done(); 
            }
 
