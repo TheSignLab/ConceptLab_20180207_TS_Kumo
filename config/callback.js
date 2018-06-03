705008a329101a09b2ca2d5c536529b40d4e9881
TheSignLab Co <34943861+TheSignLab@users.noreply.github.com>
2018-06-03 17:36:06 -0500

    sdfsdfs
    sdfs

--- config/callback.js
+++ config/callback.js
@@ -1,6 +1,40 @@
 module.exports = {
 
-    var callback: function () {
+    processingError: function (error, done) {
+        console.log(error);
+    },
 
+
+    processingSuccessfull: function (res, done) {
+        var $ = res.$;
+
+        var hrefs = [];
+        var href = "";
+        
+        var nextPage =  $(".b a").attr("href");
+
+        $(".main-results a").each(function (i, elem) {
+            href = $(this).attr("href");
+            if (href) {
+                hrefs.push(href);
+            }
+
+        });
+        
+        
+        
+        var output = {
+            hrefs : hrefs,
+            nextPage : nextPage
+        }
+        return output;
     }
+
+
+
+
+
+
+
+
 };
