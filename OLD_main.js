211ac9aaa9f8989d50685ec65bba290003236d4c
TheSignLab Co <34943861+TheSignLab@users.noreply.github.com>
2018-06-22 11:54:17 -0500

    vhj

---
+++ OLD_main.js
@@ -0,0 +1,262 @@
+/*
+    ---------------------------------------------
+    
+        Directorio Judicial Scrapper 2018 
+        
+    ---------------------------------------------
+*/
+
+
+
+
+
+
+// ----------------------------------------------- //
+var db = [];
+var init_uri = 'http://busquedas.dafp.gov.co/search?btnG=Buscar&client=Hojas_de_vida&output=xml_no_dtd&proxystylesheet=Hojas_de_vida&sort=date:D:L:d1&oe=UTF-8&ie=UTF-8&ud=1&getfields=*&wc=200&wc_mc=1&lr=lang_es&exclude_apps=1&site=Hojas_de_Vida&filter=0&entqr=3&ulang=es&ip=186.84.41.105&access=p&entqrm=0&q=bogota+inmeta:DPTO%3DBogotá%2520D%252EC&dnavs=inmeta:DPTO%3DBogotá%2520D%252EC&start=1';
+var userCodes = ["M605691-0004-4/view",
+               "M130558-0020-4/view",
+               "M1019272-0296-4/view",
+               "M97057-0296-4/view",
+               "M41433-0296-4/view",
+               "M114484-6194-4/view"];
+// ----------------------------------------------- //
+
+
+
+
+// ----------------------------------------------- //
+
+const fs = require('fs');
+const cw = require('crawler');
+const seeds = require('./seeds.json');
+
+const base_url = "http://www.sigep.gov.co/hdv/-/directorio/";
+const json_file = "exports/persons.json";
+
+var state = [];
+// ----------------------------------------------- //
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ----------------------------------------------- //
+function getUserCodeByKeyword(keyword, callback_p) {
+    c.queue([{
+        uri: init_uri,
+        callback: function (error, res, done) {
+            if (error) {
+                console.log(error);
+            } else {
+                var $ = res.$;
+                $(".main-results a").each(function (i, elem) {
+                    var _href = $(this).attr("href").replace("http://www.sigep.gov.co/hdv/-/directorio/", "");
+                    userCodes.push(_href);
+                });
+                callback_p(userCodes);
+            }
+            done();
+        }
+    }]);
+}
+
+function getUserDataByUserCode(usercode, callback) {
+    var obj = {
+        name: "",
+        position: "",
+        institution: "",
+        email: "",
+        phone: [],
+        website: "",
+        academic: [],
+        work: []
+    };
+
+
+    callback(obj)
+}
+
+function getInfo(res) {
+
+    var $ = res.$;
+    var _obj = {
+        name: "",
+        position: "",
+        institution: "",
+        email: "",
+        phone: [],
+        website: "",
+        academic: [],
+        work: []
+    };
+    var c_name = $(".nombre_funcionario");
+    var c_position = $(".cargo_funcionario");
+    var c_institution = $(".institucion_funcionario");
+    var c_contacto = $(".texto_detalle_directorio");
+    var c_academic = $(".zona_directorio_detail ul li")
+    var c_work = $("table");
+
+    _obj.name = c_name.text();
+    _obj.position = c_position.text();
+    _obj.institution = c_institution.text();
+
+    c_contacto.each(function (i, elem) {
+        var p = $(this).text();
+        if (p.includes("@")) {
+            _obj.email = p;
+        } else {
+            _obj.phone.push(p);
+        }
+    });
+
+    c_academic.each(function (i, elem) {
+        var p = $(this).text();
+        _obj.academic.push(p);
+    });
+
+    c_work.each(function (i, elem) {
+        var p = $(this).text();
+        if (!p.includes("Rangos de salario por nivel")) {
+            $(this).find("tr").each(function (i, elem) {
+                var tds = $(this).find("td");
+                if (tds.eq(0).text() != "Cargos") {
+                    var job = {
+                        description: tds.eq(0).text(),
+                        institution: tds.eq(1).text(),
+                        start: tds.eq(2).text(),
+                        end: tds.eq(3).text()
+                    }
+                    _obj.work.push(job);
+                }
+            });
+        }
+    });
+
+    return _obj;
+};
+
+function save_db() {
+    console.log("Guardando base de datos ....");
+    console.log(" ");
+    console.log(" ");
+    console.log(db);
+    console.log(db.length);
+
+    fs.writeFile('./exports/data.json', JSON.stringify(db, null, 2), 'utf-8', function () {});
+}
+// ----------------------------------------------- //
+
+
+
+
+
+
+
+
+
+
+
+
+// ----------------------------------------------- //
+db_k = 0;
+var c = new cw({
+    rateLimit: 1000, // `maxConnections` will be forced to 1
+    callback: function (err, res, done) {
+        obj = getInfo(res);
+        db.push(obj);
+        console.log("... Crawled " + obj.name)
+        db_k = db_k + 1;
+
+        done();
+    }
+});
+// ----------------------------------------------- //
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ----------------------------------------------- //
+    console.log('\033[2J');
+    console.log("Crawler Init ");
+    console.log(" ");
+// ----------------------------------------------- //
+
+
+
+
+
+// Loop por cada Palabra clave (Keyword) //
+for (var kSeed = 0; kSeed < seeds.length; kSeed++) {
+
+    // Funcion ejecutada por cada Keyword , Retorna lista de Usuarios por Keyword //
+    getUserCodeByKeyword(seeds[kSeed], function (userCodeList) {
+
+        // Loop por toda la lista de Ususario para esta Keyword //
+        for (var kUserCode = 0; kUserCode < userCodeList.length; kUserCode++) {
+            
+            // Actua URL
+            var _uri = base_url + userCodeList[kUserCode];
+
+            // Indicador de estado //
+            console.log("Search in " + seeds[kSeed] + " ---> UserCode : " + userCodeList[kUserCode]);
+
+            if ((kUserCode == (userCodeList.length - 1))) {
+                var _callback = function (err, res, done) {
+                    obj = getInfo(res);
+                    db.push(obj);
+                    done();
+                }
+                var options = [{
+                    uri: _uri,
+                    callback: function (error, res, done) {
+                        if (error) {
+                            console.log(error);
+                        } else {
+                            _callback(error, res, done);
+                        }
+                        done();
+                    }
+                }];
+                c.queue(options);
+            } else {
+                c.queue(_uri);
+            }
+        }
+    });
+    
+    if(kSeed == (seeds.length - 1)){
+        
+    }
+
+}
--- main.js
+++ main.js
@@ -12,251 +12,129 @@
 
 
 // ----------------------------------------------- //
-var db = [];
-var init_uri = 'http://busquedas.dafp.gov.co/search?btnG=Buscar&client=Hojas_de_vida&output=xml_no_dtd&proxystylesheet=Hojas_de_vida&sort=date:D:L:d1&oe=UTF-8&ie=UTF-8&ud=1&getfields=*&wc=200&wc_mc=1&lr=lang_es&exclude_apps=1&site=Hojas_de_Vida&filter=0&entqr=3&ulang=es&ip=186.84.41.105&access=p&entqrm=0&q=bogota+inmeta:DPTO%3DBogotá%2520D%252EC&dnavs=inmeta:DPTO%3DBogotá%2520D%252EC&start=1';
-var userCodes = ["M605691-0004-4/view",
-               "M130558-0020-4/view",
-               "M1019272-0296-4/view",
-               "M97057-0296-4/view",
-               "M41433-0296-4/view",
-               "M114484-6194-4/view"];
-// ----------------------------------------------- //
 
-
-
-
-// ----------------------------------------------- //
-
+// Require External Scripts
 const fs = require('fs');
 const cw = require('crawler');
 const seeds = require('./seeds.json');
 
-const base_url = "http://www.sigep.gov.co/hdv/-/directorio/";
-const json_file = "exports/persons.json";
+//Storage File
+var db_all = [];
+var db_usercode = [];
 
-var state = [];
+//Init URL Strings
+const URL_Base = 'http://busquedas.dafp.gov.co';
+const URL_Query = '/search?q=cti&btnG=Buscar&client=Hojas_de_vida&output=xml_no_dtd&proxystylesheet=Hojas_de_vida&sort=date%3AD%3AL%3Ad1&oe=UTF-8&ie=UTF-8&ud=1&getfields=*&wc=200&wc_mc=1&lr=lang_es&exclude_apps=1&site=Hojas_de_Vida&getfields=*&filter=0&lr=lang_es&entqr=';
+const URL_Init = URL_Base + URL_Query + '1';
+
+
+const UserCode_URL_Base = "http://www.sigep.gov.co/hdv/-/directorio/";
+const Persons_FilePath = "exports/persons.json";
 // ----------------------------------------------- //
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // ----------------------------------------------- //
-function getUserCodeByKeyword(keyword, callback_p) {
-    c.queue([{
-        uri: init_uri,
-        callback: function (error, res, done) {
-            if (error) {
-                console.log(error);
-            } else {
-                var $ = res.$;
-                $(".main-results a").each(function (i, elem) {
-                    var _href = $(this).attr("href").replace("http://www.sigep.gov.co/hdv/-/directorio/", "");
-                    userCodes.push(_href);
-                });
-                callback_p(userCodes);
-            }
-            done();
-        }
-    }]);
-}
-
-function getUserDataByUserCode(usercode, callback) {
-    var obj = {
-        name: "",
-        position: "",
-        institution: "",
-        email: "",
-        phone: [],
-        website: "",
-        academic: [],
-        work: []
-    };
-
-
-    callback(obj)
-}
-
-function getInfo(res) {
-
-    var $ = res.$;
-    var _obj = {
-        name: "",
-        position: "",
-        institution: "",
-        email: "",
-        phone: [],
-        website: "",
-        academic: [],
-        work: []
-    };
-    var c_name = $(".nombre_funcionario");
-    var c_position = $(".cargo_funcionario");
-    var c_institution = $(".institucion_funcionario");
-    var c_contacto = $(".texto_detalle_directorio");
-    var c_academic = $(".zona_directorio_detail ul li")
-    var c_work = $("table");
-
-    _obj.name = c_name.text();
-    _obj.position = c_position.text();
-    _obj.institution = c_institution.text();
-
-    c_contacto.each(function (i, elem) {
-        var p = $(this).text();
-        if (p.includes("@")) {
-            _obj.email = p;
-        } else {
-            _obj.phone.push(p);
-        }
-    });
-
-    c_academic.each(function (i, elem) {
-        var p = $(this).text();
-        _obj.academic.push(p);
-    });
-
-    c_work.each(function (i, elem) {
-        var p = $(this).text();
-        if (!p.includes("Rangos de salario por nivel")) {
-            $(this).find("tr").each(function (i, elem) {
-                var tds = $(this).find("td");
-                if (tds.eq(0).text() != "Cargos") {
-                    var job = {
-                        description: tds.eq(0).text(),
-                        institution: tds.eq(1).text(),
-                        start: tds.eq(2).text(),
-                        end: tds.eq(3).text()
-                    }
-                    _obj.work.push(job);
-                }
-            });
-        }
-    });
-
-    return _obj;
-};
-
-function save_db() {
-    console.log("Guardando base de datos ....");
-    console.log(" ");
-    console.log(" ");
-    console.log(db);
-    console.log(db.length);
-
-    fs.writeFile('./exports/data.json', JSON.stringify(db, null, 2), 'utf-8', function () {});
-}
-// ----------------------------------------------- //
-
-
-
-
-
-
-
-
-
-
-
-
-// ----------------------------------------------- //
-db_k = 0;
-var c = new cw({
+var keywordsCrawlerOptions = {
     rateLimit: 1000, // `maxConnections` will be forced to 1
     callback: function (err, res, done) {
-        obj = getInfo(res);
-        db.push(obj);
-        console.log("... Crawled " + obj.name)
-        db_k = db_k + 1;
-
         done();
     }
-});
+}
+var c = new cw(keywordsCrawlerOptions);
 // ----------------------------------------------- //
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 // ----------------------------------------------- //
-    console.log('\033[2J');
-    console.log("Crawler Init ");
-    console.log(" ");
+console.log('\033[2J');
+console.log("Crawler Init ");
+console.log(" ");
 // ----------------------------------------------- //
-
-
 
 
 
 // Loop por cada Palabra clave (Keyword) //
 for (var kSeed = 0; kSeed < seeds.length; kSeed++) {
 
-    // Funcion ejecutada por cada Keyword , Retorna lista de Usuarios por Keyword //
-    getUserCodeByKeyword(seeds[kSeed], function (userCodeList) {
+    var thisKeyword = seeds[kSeed];
 
-        // Loop por toda la lista de Ususario para esta Keyword //
-        for (var kUserCode = 0; kUserCode < userCodeList.length; kUserCode++) {
-            
-            // Actua URL
-            var _uri = base_url + userCodeList[kUserCode];
-
-            // Indicador de estado //
-            console.log("Search in " + seeds[kSeed] + " ---> UserCode : " + userCodeList[kUserCode]);
-
-            if ((kUserCode == (userCodeList.length - 1))) {
-                var _callback = function (err, res, done) {
-                    obj = getInfo(res);
-                    db.push(obj);
-                    done();
-                }
-                var options = [{
-                    uri: _uri,
-                    callback: function (error, res, done) {
-                        if (error) {
-                            console.log(error);
-                        } else {
-                            _callback(error, res, done);
-                        }
-                        done();
-                    }
-                }];
-                c.queue(options);
-            } else {
-                c.queue(_uri);
-            }
-        }
-    });
-    
-    if(kSeed == (seeds.length - 1)){
-        
+    // Normal Keywords
+    if (kSeed < (seeds.length - 1)) {
+        console.log("Keyword : " + thisKeyword)
+        keywordsCallback(URL_Init, thisKeyword, function (userKeyword, userCodes, next) {
+            processingPage(userKeyword, userCodes, next);
+        });
+    } else {
+        console.log("Last Keyword : " + thisKeyword);
+        console.log(" - ");
+        console.log(" - ");
     }
 
+}
+
+
+
+function keywordsCallback(url, keyword, keywordCallback) {
+    c.queue([{
+        uri: url,
+        callback: function (error, res, done) {
+            if (error) {
+                console.log(error);
+            } else {
+                var $ = res.$;
+                var list = [];
+                $(".main-results a").each(function (i, elem) {
+                    var _href = $(this).attr("href").replace("http://www.sigep.gov.co/hdv/-/directorio/", "");
+                    list.push(_href);
+                });
+                var next = "";
+                next = URL_Base + $('a[ctype="nav.next"]').attr("href");
+                keywordCallback(keyword, list, next);
+            }
+            done();
+        }
+    }]);
+}
+
+
+function processingPage(userKeyword, userCodes, next) {
+    // Loop por toda la lista de Ususario para esta Keyword //
+    for (var kUserCode = 0; kUserCode < userCodes.length; kUserCode++) {
+
+        // Actua URL
+        var thisUserCodeURL = UserCode_URL_Base + userCodes[kUserCode];
+
+        if (kUserCode < (userCodes.length - 1)) {
+            db_usercode.push(thisUserCodeURL);
+        } else {
+            db_usercode.push(thisUserCodeURL);
+            if (!next.includes("undefined")) {
+                keywordsCallback(next, userKeyword, function (userKeyword, userCodes, next) {
+                    processingPage(userKeyword, userCodes, next);
+                });
+                var nPage = next.replace("http://busquedas.dafp.gov.co/search?q=cti&btnG=Buscar&client=Hojas_de_vida&output=xml_no_dtd&proxystylesheet=Hojas_de_vida&sort=date:D:L:d1&oe=UTF-8&ie=UTF-8&ud=1&getfields=*&wc=200&wc_mc=1&lr=lang_es&exclude_apps=1&site=Hojas_de_Vida&filter=0&entqr=1&ulang=&ip=190.147.24.37&access=p&entqrm=0&start=", "");
+                console.log(nPage)
+            } else {
+                run_usercrawler();
+            }
+
+        }
+    };
+}
+
+
+
+function run_usercrawler() {
+    console.log("Ahora si Perras");
+    var jsonfile = require('jsonfile')
+
+    var file = './exports/data_usercode.json'
+    var obj = db_usercode;
+    jsonfile.writeFile(file, obj, {
+        spaces: 2,
+        EOL: '\r\n'
+    }, function (err) {
+        console.error(err)
+    })
 }
